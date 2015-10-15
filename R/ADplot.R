772b312fc4799d30372e98565a485a528283cc36
Sacha Epskamp <sachaepskamp@vpn-staff-146-50-69-49.vpn.uva.nl>
2015-10-15 13:16:15 +0800

    ADplot no longer defaults to using qgraphAnnotate

--- R/ADplot.R
+++ R/ADplot.R
@@ -1,4 +1,4 @@
-ADplot <- function(x, filename = "ADplot", clusterID)
+ADplot <- function(x, filename = "ADplot", clusterID, annotate = FALSE)
 {
   eqMat <- outer(x$authorID, x$authorID, '==')
   
@@ -15,16 +15,25 @@ ADplot <- function(x, filename = "ADplot", clusterID)
   edgeList <- which(eqMat, arr.ind = TRUE)
   edgeList <- edgeList[edgeList[,1] > edgeList[,2],]
   
-  Graph <- qgraph(edgeList, repulsion = 0.8, DoNotPlot = TRUE, groups = coms, legend = FALSE, esize = 1,
-                  nNodes = nrow(x), edgelist = TRUE, directed = FALSE)
-  
-  qgraphAnnotate(Graph, 
-                 Name = x$original_fullName, 
-                 Authors =  x$AF,
-                 Year =  x$PY,
-                 Title =  gsub("[\'\"]","",x$TI),
-                 authorID = x$authorID,
-                 cluster = clusterID,
-                 Address = gsub("[\'\"]","",x$address), fromqgraph = FALSE, filename = filename, image.size = "1200x1200")  
+ 
+  if (annotate){
+    
+    Graph <- qgraph(edgeList, repulsion = 0.8, DoNotPlot = TRUE, groups = coms, legend = FALSE, esize = 1,
+                    nNodes = nrow(x), edgelist = TRUE, directed = FALSE)
+    
+    qgraphAnnotate(Graph, 
+                   Name = x$original_fullName, 
+                   Authors =  x$AF,
+                   Year =  x$PY,
+                   Title =  gsub("[\'\"]","",x$TI),
+                   authorID = x$authorID,
+                   cluster = clusterID,
+                   Address = gsub("[\'\"]","",x$address), fromqgraph = FALSE, filename = filename, image.size = "1200x1200")  
+    
+  } else {
+    
+    qgraph(edgeList, repulsion = 0.8, groups = coms, legend = FALSE, esize = 1,
+           nNodes = nrow(x), edgelist = TRUE, directed = FALSE)
+  }
   
 }
